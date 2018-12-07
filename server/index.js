0073b5d88f4b8946a184036d2834649a85d1adb9
Homère Faivre <homere.faivre@COMP10672.local>
2018-12-07 16:58:01 +0100

    added DD agent container for monitoring

--- server/index.js
+++ server/index.js
@@ -40,15 +40,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
-  res.send(values.rows);
+  try {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+  } catch (error) {
+    console.log(error)
+  }  
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+  redisClient
+    .hgetall('values', (err, values) => {
+      res.send(values);
+    });
 });
 
 app.post('/values', async (req, res) => {
